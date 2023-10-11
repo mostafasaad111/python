4afe564d3a9ac3d45182fc297051954f85562fc0
Mustafa Saad <73419415+mostafasaad111@users.noreply.github.com>
2023-10-11 23:52:25 +0300

    Add files via upload

---
+++ 04_03.py
@@ -0,0 +1,11 @@
+print("hi")
+
+name = input("what's your name?")
+print("it's nice to meet you, ", name)
+
+answer = input("are you enjoying the course?")
+
+if answer == "yes":
+    print("that's good to hear!")
+
+print("final statement");
