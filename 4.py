8ddcfd1786d0d336263124b1ee7c29747c49656c
Mustafa Saad <73419415+mostafasaad111@users.noreply.github.com>
2023-10-12 08:24:47 +0300

    List & Dictionary

---
+++ 4.py
@@ -0,0 +1,25 @@
+# collections
+# list
+color1 = "red"
+color2 = "green "
+color3 = "blue"
+color4 = "yellow"
+print(color1)
+
+colorList = ["yellow", "green", "blue", "black"]
+print(colorList[3])
+# dictionary
+
+mustafa="010323934"
+mohamed="012323934"
+ali="013323934"
+sad="015323934"
+print(mustafa)
+
+phoneBook = {
+  "mustafa" : "012323934",
+  "ali" : "012323934",
+  "sayid" : "012323934",
+  "samar" : "012323934",
+}
+print(phoneBook["mustafa"])
