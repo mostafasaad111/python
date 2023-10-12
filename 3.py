eb3eddfb5bf0de69e60947ea8a74ea123cff3b70
Mustafa Saad <73419415+mostafasaad111@users.noreply.github.com>
2023-10-12 07:32:03 +0300

    elif_statement

---
+++ 3.py
@@ -0,0 +1,12 @@
+# elif statement
+today = "saturday"
+if today == "friday":
+    print(today, "is my weekend")
+elif today == "saturday":
+    
+    print(today,"is my the day when I visit my mother")
+else:
+    print(today,"is a working day")
+    
+print("Thanks ")
+     
