144809e179349158d5d6268aa6f46196df343806
Mustafa Saad <73419415+mostafasaad111@users.noreply.github.com>
2023-10-16 06:56:52 +0300

    Concatenation

---
+++ Concatenation.py
@@ -0,0 +1,37 @@
+# -----------------
+# --- Concatenation----
+# --------------------
+
+msg = "I love "
+lang = "python"
+
+print(msg + " " + lang)
+
+a = "first\
+Second\
+Third\
+"
+
+b = "A \
+b\
+c"
+
+print(a + "\n" + b)
+
+######################  Elzero asignments #######################
+name = "mustafa"
+age = "20"
+country = "mit-gamer"
+
+print(type(name))
+print(type(age))
+print(type(country))
+
+var = {"Name": "Osama" , "Age": "38" ,"Country": "Egypt"}
+print(var)
+###############################################################
+name2 = "Osama"
+age = "38" 
+country = "Egypt"
+print("Hello, My Name Is " + name2+ "and Iam" +age+ "Years old and I live in " +country)
+###########################################################3333333
