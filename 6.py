4aa357293954686642758ff9d8540fe27684c19f
Mustafa Saad <73419415+mostafasaad111@users.noreply.github.com>
2023-10-16 06:26:00 +0300

    Data Type

---
+++ 6.py
@@ -0,0 +1,55 @@
+# Data type
+
+print(type(10))  # int
+
+print(type(10.5))  # float
+
+print(type("Mustafa")) #string
+print(type("Hello python")) #string
+
+print(type([1,2,3,4,5,6]))  #list 
+
+print(type((1,2,3,4,5,6))) #tuple
+
+print(type({"one":1, "two":2})) #dictionary
+
+print(type(2 == 2)) #boolean
+
+_myVariable = "sad"
+print(_myVariable)
+
+a,b,c = 1,2,4
+print(a)
+print(c)
+print(b)
+
+# Escape Sequence Characters
+#\b => Back Space   \b
+print("Hello\bWorld")  #will Remove o
+
+# Escape newLines  \newLine
+print("hello \
+  i LOVE \
+  PYTHON")    
+
+# Escape Back Slash \\
+print("I love Back Slash \\  ")
+
+
+# Escape Single Quote  \'
+print('I love Back Slash \'test\' ')
+
+#Escape Double Quote  \""
+print("I love Back Slash \"test\" ")
+
+#Line Feed \n
+print("hello world\nsecond")
+
+#Carriage Return  \r
+print("12333333\rk")
+
+#Horizontal Tab   \t
+print("hello\tworld")
+
+#Character hexaValue \xhh
+print("\x73")
