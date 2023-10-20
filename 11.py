22ce4a3a7b14b607b2ca005de80f3daf54feafaf
Mustafa Saad <73419415+mostafasaad111@users.noreply.github.com>
2023-10-20 11:00:04 +0300

    Numbers

---
+++ 11.py
@@ -0,0 +1,38 @@
+#######--------numbers-------#####
+#integer 
+print(type(34))
+print(type(13))
+print(type(-100))
+print(type(-10))
+print(type(100))
+#float
+print(type(1.3345))
+print(type(1.3345))
+print(type(-1.3345))
+
+#complex numbers
+
+myNumber = 5+6j
+print("real part is {}".format(myNumber.real))
+print("real part is {}".format(myNumber.imag))
+print(type(5+6j))
+
+
+print(100)
+print(float(100))
+print(complex(100))
+
+print(10.4)
+print(int(10.4))
+
+
+##------Arithmetic operations -----------#
+#  ( + , - , * , / , % , ** , // )
+print(  10 + 20 )
+print(  10 - 20 )
+print(  10 * 20 )
+print(  10 / 20 )
+print(  24 % 20 )
+print(  2**3 )
+print(119 // 20)
+print(120 // 20)
