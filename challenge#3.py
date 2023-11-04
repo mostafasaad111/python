eb4288a3dcfac44390ff22fe63f488fb20f937b2
Mustafa Saad <73419415+mostafasaad111@users.noreply.github.com>
2023-11-04 14:37:38 +0200

    challenge#3

---
+++ challenge#3.py
@@ -0,0 +1,11 @@
+def calculate(num1, num2, num3):
+    return (num1 + num2) * num3
+
+
+print(calculate(2, 3, 4))
+print(calculate(4, 3, 4))
+print(calculate(2, 9, 4))
+# input("press any key to exit.........")
+
+
+
